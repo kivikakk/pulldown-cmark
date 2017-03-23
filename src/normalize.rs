3778386ceeead5a91c0271325e2d686a0106150a
Yuki Izumi <kivikakk@github.com>
2017-03-23 14:19:05 +1100

    Normalize whitespace surrounding block tags.

--- src/normalize.rs
+++ src/normalize.rs
@@ -1,5 +1,6 @@
 use std::default::Default;
 use std::fmt::Write;
+use std::collections::HashSet;
 
 use regex::Regex;
 use html5ever::tendril::TendrilSink;
@@ -22,23 +23,44 @@ pub fn normalize(s: &str) -> String {
         .from_utf8()
         .one(s.as_bytes());
 
-    process(&mut *dom.document.borrow_mut());
+    process(&mut *dom.document.borrow_mut(), None, None);
 
     let mut res = vec![];
     serialize(&mut res, &dom.document, SerializeOpts::default()).unwrap();
     String::from_utf8(res).unwrap()
 }
 
-fn process(n: &mut Node) {
+fn process(n: &mut Node, prev_el: Option<String>, next_el: Option<String>) {
     lazy_static! {
         static ref WHITESPACE_RE: Regex = Regex::new(r"\s+").unwrap();
+        static ref LEADING_WHITESPACE_RE: Regex = Regex::new(r"\A\s+").unwrap();
+        static ref TRAILING_WHITESPACE_RE: Regex = Regex::new(r"\s+\z").unwrap();
+        static ref BLOCK_TAGS: HashSet<&'static str> =
+            ["article", "header", "aside", "hgroup", "blockquote", "hr", "iframe", "body", "li",
+            "map", "button", "object", "canvas", "ol", "caption", "output", "col", "p", "colgroup",
+            "pre", "dd", "progress", "div", "section", "dl", "table", "td", "dt", "tbody", "embed",
+            "textarea", "fieldset", "tfoot", "figcaption", "th", "figure", "thead", "footer", "tr",
+            "form", "ul", "h1", "h2", "h3", "h4", "h5", "h6", "video", "script", "style"]
+                .iter()
+                .cloned()
+                .collect();
     }
 
     match n.node {
         NodeEnum::Document => { },
         NodeEnum::Doctype(..) => { },
         NodeEnum::Text(ref mut t) => {
-            let s = &*WHITESPACE_RE.replace_all(&**t, " ").into_owned();
+            let mut s = WHITESPACE_RE.replace_all(&**t, " ").into_owned().to_string();
+            if let Some(prev_el_name) = prev_el {
+                if BLOCK_TAGS.contains(&*prev_el_name) {
+                    s = LEADING_WHITESPACE_RE.replace_all(&s, "").into_owned();
+                }
+            }
+            if let Some(next_el_name) = next_el {
+                if BLOCK_TAGS.contains(&*next_el_name) {
+                    s = TRAILING_WHITESPACE_RE.replace_all(&s, "").into_owned();
+                }
+            }
             t.clear();
             t.write_str(&s).unwrap();
         },
@@ -51,7 +73,17 @@ fn process(n: &mut Node) {
         },
     }
 
-    for h in &n.children {
-        process(&mut *h.borrow_mut());
+    let len = n.children.len();
+    for i in 0..len {
+        let prev = if i == 0 { None } else { get_element(&n.children[i-1].borrow().node) };
+        let next = if i == len - 1 { None } else { get_element(&n.children[i+1].borrow().node) };
+        process(&mut *n.children[i].borrow_mut(), prev, next);
+    }
+}
+
+fn get_element(node: &NodeEnum) -> Option<String> {
+    match node {
+        &NodeEnum::Element(ref qn, _, _) => Some((&*qn.local).to_string().to_lowercase()),
+        _ => None,
     }
 }
